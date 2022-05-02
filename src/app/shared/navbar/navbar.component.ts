f65821b5afebdc9692daf62b3787173dceb64356
Leandro <leandro.n.aguilera@gmail.com>
2022-05-01 23:24:32 -0300

    Scroll a la seccion solicitada

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toAbout() {
+    document.getElementById('about')?.scrollIntoView({ behavior: "smooth" });
+  }
+
+  toSkills() {
+    document.getElementById('skills')?.scrollIntoView({ behavior: "smooth" });
+  }
+
+  toTrabajos() {
+    document.getElementById('trabajos')?.scrollIntoView({ behavior: "smooth" });
+  }
+
+  toContacto() {
+    document.getElementById('contacto')?.scrollIntoView({ behavior: "smooth" });
+  }
 }
