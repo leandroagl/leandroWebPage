e79f13188d74bf7ccbaf081f9b52c98b27d07d8b
Leandro <leandro.n.aguilera@gmail.com>
2022-05-12 21:30:02 -0300

    refactor, url como parametro

--- src/app/landing/trabajos/card/card.component.ts
+++ src/app/landing/trabajos/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit, } from '@angular/core';
+import { RouteConfigLoadEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -10,15 +11,25 @@ export class CardComponent implements OnInit {
   @Input() src : string = '';
   @Input() backgroundUrl: string = '';
   @Input() cardTitle: string ='';
-  @Input() cardText: string = ''
+  @Input() cardText: string = '';
+  @Input() navigateLink: string = '';
 
 
-  constructor() {
+  constructor( private router: Router ) {
   }
 
 
   ngOnInit(): void {
 
   }
+
+  navigateTo() {
+    this.router.navigateByUrl('').then(
+      result => {
+        window.location.href = this.navigateLink;
+      }
+    )
+  }
+
 
 }
